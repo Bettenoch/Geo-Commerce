4166d732f5442373a86450ca666b6b46dcc5bf9e
Bettenoch <bettenoch254@gmail.com>
2024-01-16 18:44:23 +0300

    Run migrations.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api",
     # 3rd party apps
     "rest_framework",
     "corsheaders",
